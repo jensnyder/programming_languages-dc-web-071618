d7bdd6878a077eb4cadde3b53da933e1eb359786
Jennifer Snyder <jlsnyder09@gmail.com>
2018-06-30 14:36:49 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -37,11 +37,11 @@ def reformat_languages(languages)
     language_data.each do |language_name, type_hash|
       type_hash.each do |type, type_name|
         new_hash[language_name] = {type: type_name}
-        if new_hash[language_name][:style] == nil
-          new_hash[language_name][:style] = [style]
-        else
-          new_hash[language_name][:style] << style
-        end
+      end
+      if new_hash[language_name][:style] == nil
+        new_hash[language_name][:style] = [style]
+      else
+        new_hash[language_name][:style] << style
       end
     end
   end
